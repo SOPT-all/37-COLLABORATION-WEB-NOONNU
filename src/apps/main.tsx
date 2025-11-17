e1aab9eeb970456dd06cad393b6c845a81729530
wonpark1 <2023111927@dgu.ac.kr>
2025-11-18 06:24:03 +0900

    chore: query 로직 분리

--- src/apps/main.tsx
+++ src/apps/main.tsx
@@ -1,20 +1,11 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import App from "./App.tsx";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnWindowFocus: false,
-      staleTime: 1000 * 60,
-    },
-  },
-});
+import { queryClient } from "../shared/apis/query-client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
