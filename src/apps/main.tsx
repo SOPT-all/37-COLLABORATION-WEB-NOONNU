120f334d60d4de14249b66be96fa4dee00f456e2
wonpark1 <2023111927@dgu.ac.kr>
2025-11-18 06:19:49 +0900

    chore: main.tsx 충돌 해결

--- src/apps/main.tsx
+++ src/apps/main.tsx
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-=======
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './App.tsx';
->>>>>>> develop
 
 import App from "./App.tsx";
 
@@ -24,14 +18,9 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<<<<<<< HEAD
     <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
-=======
-    <App />
-  </StrictMode>,
->>>>>>> develop
 );
