585e6f7b1391f1b5fb6cb8316650a948a216ee77
wonpark1 <2023111927@dgu.ac.kr>
2025-11-18 06:27:04 +0900

    chore: instance.ts 관심사 분리

--- src/shared/apis/instance.ts
+++ src/shared/apis/instance.ts
@@ -1,29 +1,5 @@
 import axios from "axios";
 
-export const apiClient = axios.create({
+export const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
-
-export const get = async <T>(...args: Parameters<typeof apiClient.get>) => {
-  try {
-    const response = await apiClient.get<T>(...args);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-export const post = async <T, B = unknown>(
-  url: string,
-  body?: B,
-  config?: Parameters<typeof apiClient.post>[2]
-) => {
-  try {
-    const response = await apiClient.post<T>(url, body, config);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
