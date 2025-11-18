431a8e01baa8038fb8ef2161a2c1f48217430e43
eunkr82 <eun.kr82@gmail.com>
2025-11-19 01:09:53 +0900

    init: 절대경로 초기 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+    },
+  },
+
   plugins: [react()],
 });
