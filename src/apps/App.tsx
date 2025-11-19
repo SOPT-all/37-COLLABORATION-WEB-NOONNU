dddfcc072f8c6bfe48ae0d13b15ed84024924090
twossu <nadao202@naver.com>
2025-11-19 21:05:30 +0900

    fix: app.tsx에 QueryClientProvider, RouterProvider, ReactQueryDevtools 설정

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,12 +1,22 @@
-import ThemeProvider from '../shared/styles/theme-provider';
-import { rootStyle } from '../shared/styles/global.css';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider } from 'react-router';
+
+import { router } from '@/router/router';
+import { queryClient } from '@/shared/apis/query-client';
+import { rootStyle } from '@/shared/styles';
+import ThemeProvider from '@/shared/styles/theme-provider';
 
 function App() {
   return (
-    <ThemeProvider className={rootStyle}>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider className={rootStyle}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
