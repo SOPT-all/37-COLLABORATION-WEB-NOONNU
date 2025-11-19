cd4d9cdcfd78375b69e5f36cd23e977696a487aa
twossu <nadao202@naver.com>
2025-11-19 15:05:21 +0900

    fix: 예시 css 삭제

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,20 +1,11 @@
 import ThemeProvider from '../shared/styles/theme-provider';
-import * as styles from './app.css';
 import { rootStyle } from '../shared/styles/global.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
     <ThemeProvider className={rootStyle}>
-      <p className={styles.display36b}>display-36b</p>
-      <p className={styles.display20r}>display-20r</p>
-      <p className={styles.header16m}>header-16m</p>
-      <p className={styles.title28sb}>title-28sb</p>
-      <p className={styles.body20sb}>body-20sb</p>
-      <p className={styles.body18sb}>body-18sb</p>
-      <p className={styles.body16sb}>body-16sb</p>
-      <p className={styles.body16m}>body-16m</p>
-      <p className={styles.body14m}>body-14m</p>
-      <p className={styles.body14r}>body-14r</p>
+      <ReactQueryDevtools initialIsOpen={false} />
     </ThemeProvider>
   );
 }
