cc7f978aef53c1292b62a354dbad1945113f0c0d
JS MacBook Pro <j_seokpark@naver.com>
2025-11-21 15:35:07 +0900

    feat: 테스트 페이지 초기화

--- src/pages/free-font/free-font.tsx
+++ src/pages/free-font/free-font.tsx
@@ -1,21 +1,5 @@
-import ListView from '@/shared/components/list-view/list-view';
-
-const data = {
-  id: 1,
-  name: '프리텐다드',
-  producer: '눈누',
-  thicknessNum: 3,
-  phrase: '샘플 텍스트입니다',
-  isLiked: false,
-  isCompared: false,
-};
-
 const FreeFont = () => {
-  return (
-    <div style={{ width: '954px', height: '160px', margin: '100px 100px' }}>
-      <ListView {...data} />
-    </div>
-  );
+  return <div>무료폰트 페이지</div>;
 };
 
 export default FreeFont;
