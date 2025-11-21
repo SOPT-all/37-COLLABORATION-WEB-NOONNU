0de202884048fc980793ba533d791a266c12c666
JS MacBook Pro <j_seokpark@naver.com>
2025-11-21 12:55:22 +0900

    refactor: 중복 스타일 통합

--- src/widgets/free-font/components/compare-floating-button/compare-floating-button.css.ts
+++ src/widgets/free-font/components/compare-floating-button/compare-floating-button.css.ts
@@ -1,26 +1,31 @@
 import { keyframes, style } from '@vanilla-extract/css';
 
 import { themeVars } from '@/shared/styles';
-
-export const compareButton = style({
+const baseButtonStyle = style({
   display: 'flex',
-  width: '18.8rem',
-  height: '7.6rem',
+  width: '100%',
+  height: '100%',
   padding: '2.4rem 3.2rem',
   justifyContent: 'center',
   borderRadius: '99px',
-  border: `1px solid ${themeVars.color.primary_dark_01}`,
-  backgroundColor: themeVars.color.primary,
-  gap: '0.4rem',
   cursor: 'pointer',
   whiteSpace: 'nowrap',
   ...themeVars.fontStyles.body_20sb,
-
-  ':hover': {
-    backgroundColor: themeVars.color.primary_dark_01,
-    borderColor: themeVars.color.primary_dark_02,
-  },
 });
+
+export const compareButton = style([
+  baseButtonStyle,
+  {
+    border: `1px solid ${themeVars.color.primary_dark_01}`,
+    backgroundColor: themeVars.color.primary,
+    gap: '0.4rem',
+
+    ':hover': {
+      backgroundColor: themeVars.color.primary_dark_01,
+      borderColor: themeVars.color.primary_dark_02,
+    },
+  },
+]);
 
 export const arrowIconStyle = style({
   maxWidth: 0,
@@ -51,16 +56,11 @@ export const animateText = style({
   animation: `${slideUp} 0.3s ease`,
 });
 
-export const emptyButton = style({
-  display: 'flex',
-  width: '18.8rem',
-  height: '7.6rem',
-  padding: '2.4rem 3.2rem',
-  justifyContent: 'center',
-  borderRadius: '99px',
-  border: `1px solid ${themeVars.color.gray_03}`,
-  backgroundColor: themeVars.color.gray_02,
-  cursor: 'pointer',
-  color: themeVars.color.gray_04,
-  ...themeVars.fontStyles.body_20sb,
-});
+export const emptyButton = style([
+  baseButtonStyle,
+  {
+    border: `1px solid ${themeVars.color.gray_03}`,
+    backgroundColor: themeVars.color.gray_02,
+    color: themeVars.color.gray_04,
+  },
+]);
