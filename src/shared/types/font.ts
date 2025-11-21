b818a24ac9b53520df6a2657e63f938f2b60150a
조혜린 <164613169+jogpfls@users.noreply.github.com>
2025-11-21 13:14:27 +0900

    [Feat] FontFace 기반 동적 폰트 로딩 시스템 구축 (#41)
    * feat: mock 데이터, 타입 설정
    
    * feat: 폰트 동적 로딩을 위한 font-loader 및 use-load-font 훅 구현
    
    * chore: vanila-extract/dynamic 라이브러리 설치
    
    * fix: 파일명 변경

---
+++ src/shared/types/font.ts
@@ -0,0 +1,19 @@
+export interface FontMetadataType {
+  fontFamily: string;
+  fontSource: string;
+  fontWeight: string;
+  fontDisplay: FontDisplay;
+}
+
+export interface FontItemType {
+  id: number;
+  name: string;
+  producer: string;
+  thicknessNum: number;
+  phrase: string;
+  isLiked: boolean;
+  isCompared: boolean;
+  fontMetadata: FontMetadataType;
+}
+
+export type FontDisplay = 'auto' | 'block' | 'swap' | 'fallback' | 'optional';
