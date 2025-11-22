59ccb6d62019ded0c6fd6643a639bff033696e43
jogpfls <joerin4177@gmail.com>
2025-11-23 02:20:41 +0900

    feat: layout에 header 추가

--- src/router/layout.tsx
+++ src/router/layout.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
 
+import Header from '@/shared/components/header/header';
+
 const Layout = () => {
   return (
     <div>
+      <Header />
       <Suspense fallback={<>로딩중..</>}>
         <Outlet />
       </Suspense>
+      {/* footer */}
     </div>
   );
 };
