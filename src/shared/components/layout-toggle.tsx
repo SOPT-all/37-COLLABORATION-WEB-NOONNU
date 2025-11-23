3ab5855cc04cb9c0f454a643c050e04923ba928d
eunkr82 <eun.kr82@gmail.com>
2025-11-24 00:26:39 +0900

    refactor: 컨벤션에 맞게 코드 일부 수정

--- src/shared/components/layout-toggle.tsx
+++ src/shared/components/layout-toggle.tsx
@@ -10,15 +10,15 @@ export const TOGGLE = {
 } as const;
 
 // 'grid' | 'list | 'word' 중 하나
-export type layoutToggleType = (typeof TOGGLE)[keyof typeof TOGGLE];
+export type LayoutToggleType = (typeof TOGGLE)[keyof typeof TOGGLE];
 
 //인터페이스
-interface layoutToggleProps {
-  value: layoutToggleType; // 현재 선택된 레이아웃 타입
-  onClick: (nextLayout: layoutToggleType) => void; // 레이아웃이 바뀌었음을 부모에게 알려주는 콜백
+interface LayoutToggleProps {
+  value: LayoutToggleType; // 현재 선택된 레이아웃 타입
+  onClick: (nextLayout: LayoutToggleType) => void; // 레이아웃이 바뀌었음을 부모에게 알려주는 콜백
 }
 
-export function LayoutToggle({ value, onClick }: layoutToggleProps) {
+export const LayoutToggle = ({ value, onClick }: LayoutToggleProps) => {
   const isGridSelected = value === TOGGLE.GRID;
   const isListSelected = value === TOGGLE.LIST;
   const isWordSelected = value === TOGGLE.WORD;
@@ -50,4 +50,4 @@ export function LayoutToggle({ value, onClick }: layoutToggleProps) {
       </div>
     </>
   );
-}
+};
