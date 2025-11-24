7556e13832d9635b1585a22350e99858d7b89781
wonpark1 <2023111927@dgu.ac.kr>
2025-11-25 00:00:58 +0900

    feat: free-font-button 구현

---
+++ src/shared/components/free-font-button/free-font-button.css.ts
@@ -0,0 +1,34 @@
+import { style } from '@vanilla-extract/css';
+import { themeVars } from '../../styles/theme.css';
+
+export const button = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  width: '10rem',
+  height: '4rem',
+  padding: '0.8rem 0.8rem 0.8rem 1.6rem',
+  borderRadius: '4px',
+  border: `1px solid ${themeVars.color.primary_dark_01}`,
+  backgroundColor: themeVars.color.primary,
+  cursor: 'pointer',
+
+  ':hover': {
+    opacity: 0.9,
+  },
+});
+
+export const text = style({
+  color: themeVars.color.black,
+  ...themeVars.fontStyles.body_14m,
+  whiteSpace: 'nowrap',
+});
+
+export const icon = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '2.4rem',
+  height: '2.4rem',
+});
