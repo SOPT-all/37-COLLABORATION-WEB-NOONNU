5351c22bf7f4d64699a651e4d4e0b8835428d165
twossu <nadao202@naver.com>
2025-11-26 03:40:53 +0900

    fix: 툴바 css 수정

--- src/widgets/free-font/components/font-toolbar/font-toolbar.css.ts
+++ src/widgets/free-font/components/font-toolbar/font-toolbar.css.ts
@@ -3,6 +3,6 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
   display: 'flex',
   alignItems: 'center',
-  gap: '2rem',
+  gap: '3.6rem',
   marginBottom: '1.2rem',
 });
