680d0be41c801d0ad098212de3f45cf65cb5843b
jogpfls <joerin4177@gmail.com>
2025-11-27 02:44:56 +0900

    feat: get api 연결

--- src/shared/apis/config/end-point.ts
+++ src/shared/apis/config/end-point.ts
@@ -1,5 +1,5 @@
 export const END_POINT = {
   COMPARE_STATE_CHANGE: (fontId: number) => `user/fonts/${fontId}/compare`,
-  // COMPARE_FONT: 'user/compared-fonts
+  COMPARE_FONT: 'user/compared-fonts',
   // LIKE_FONT: ...
 };
